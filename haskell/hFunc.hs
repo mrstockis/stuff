c03eda7cc7fba5ea2b7869ab1b8d16f545829990
mrstockis <mrstockis@github.com>
2018-05-31 00:33:07 +0200

    problems

--- haskell/hFunc.hs
+++ haskell/hFunc.hs
@@ -1,16 +1,23 @@
 -- Tutorial
 testList = [3,4,8,4,6]
+
 dM x = x*2
+
 dU x y = (x*2,x*y)
+
 dSN x = if x > 100 then x else x*2
 dSN' x = succ $ if x > 100 then x else x*2 
+
 cOB = "It's a-me, Conon O'Brien!"
+
 rLen :: (Num b) => [a] -> b
 rLen [] = 0
 rLen (_:xs) = 1 + rLen xs
+
 rSum :: (Num a) => [a] -> a
 rSum [] = 0
 rSum (x:xs) = x + rSum xs
+
 rPro :: (Num a) => [a] -> a
 rPro [] = 1
 rPro (x:xs) = x * rPro xs
@@ -25,6 +32,8 @@ bmiTell weight height
        normal = 25.0
        fat = 30.0
 
+
+
 -- Eulers
 euler5 q = sum $ [ x| x <- [5,10..q], x`mod`3 /= 0 ] ++ [3,6..q]
 
@@ -38,6 +47,24 @@ set' (x:xs)
  | xs == [] = [x]
  | elem x xs = x:set' [ i | i <- xs, i /= x ]
  | True = x:set' xs
+
+fib' f s r
+-- fib' f = fib' 1 1 f
+ | r == 0 = []
+ | True = f:fib' s (f+s) (r-1)
+
+qs [] = []
+qs (x:xs) = le ++ [x] ++ gt
+ where le = qs [ i | i <- xs, i <= x ]
+       gt = qs [ i | i <- xs, i > x ]
+
+qs' [] = []
+qs' (x:xs) = (qs' [i|i<-xs,i<=x]) ++ [x] ++ (qs' [i|i<-xs,i>x])
+
+sort' (x:xs)
+ | null xs = [x]
+ | null [ i | i <- xs, i < x ] = x:sort' xs
+ | True = sort' $ xs++[x]
 
 mod' :: Integral a => a -> a -> a
 mod' f s | f < s = f | True = mod' (f-s) s
@@ -54,7 +81,6 @@ max' (x:xs) | xs == [] = x
 primes (x:xs)
  | xs == [] = []
  | True = x:primes [ i | i <- xs, 0 /= rem i x ]
-
 
 -- cz :: (Integral a) => a -> [a]
 cz x|x==1=[1]|odd x=x:cz(3*x+1)|True=x:cz(div x 2)
