91c55a5d42931152b09797f87555faa9f1054103
mrstockis <mickem90@gmail.com>
2024-02-16 18:28:01 +1300

    housekeeping

--- ProjectEuler/Haskell/euler1.hs
+++
@@ -1,7 +0,0 @@
-
-import System.IO
-
-result = sum $ [ i | i <- [3,6..999], rem i 5 /= 0 ] ++ [5,10..999]
-
-main = do
- print result
--- ProjectEuler/Python/euler1.py
+++
@@ -1,6 +0,0 @@
-
-Q=1000
-
-result = sum( [ i for i in range(3,Q,3) ] + [ i for i in range(5,Q,5) if not i%3 == 0 ] )
-
-print(result)
