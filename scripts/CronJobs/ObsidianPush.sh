8fcdc12904de0bc2a06e784c911cb7402c37fa19
mrstockis <mickem90@gmail.com>
2024-12-14 10:11:58 +1300

    update

--- scripts/CronJobs/ObsidianPush.sh
+++ scripts/CronJobs/ObsidianPush.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 # cron entry:
-# 	crontab -e
-# 	0 12 * * * bash ~/CronJobs/ObsidianPush.sh
+#       crontab -e
+#       0 12 * * * bash ~/CronJobs/ObsidianPush.sh
 
-push_date=`date +%y_%m_%d`
+#push_date=`date +%y_%m_%d`
+push_date=`date +%d_%m_%y`
+
+# Log last run
+echo "$push_date" > ~/CronJobs/LogObsidianPush
+cp ~/CronJobs/LogObsidianPush ~/NAS/gitVault/
 
 git -C ~/NAS/gitVault add .
 git -C ~/NAS/gitVault commit -m "daily sync $push_date"
 git -C ~/NAS/gitVault push
-
-
